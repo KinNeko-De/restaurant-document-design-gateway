86baf6579d55927c1e89277770f880fd856ce0d9
Nils Diekmann <github@kinneko.de>
2023-07-23 11:15:11 +0200

    - ci and dockerfile

--- cmd/document-design-gateway/main.go
+++ cmd/document-design-gateway/main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kinneko-de/restaurant-document-design-gateway/build"
 	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/document"
 )
 
 func main() {
+	log.Println("Version " + build.Version)
 	router := setupRouter()
 	err := router.Run(":8080")
 	if err != nil {
