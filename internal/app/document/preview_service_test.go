111fa1971b5fa925a3e11f172d0d9ebad9970c92
Nils Diekmann <github@kinneko.de>
2023-07-30 16:24:16 +0200

    - remove non working test

--- internal/app/document/preview_service_test.go
+++ internal/app/document/preview_service_test.go
@@ -5,10 +5,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/kinneko-de/restaurant-document-design-gateway/internal/mocks"
 	"github.com/kinneko-de/restaurant-document-design-gateway/internal/testfixture"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestGeneratePreview_RequestIsNil(t *testing.T) {
@@ -19,7 +17,10 @@ func TestGeneratePreview_RequestIsNil(t *testing.T) {
 	assert.EqualValues(t, http.StatusBadRequest, response.Code)
 }
 
+/*
 func TestValid(t *testing.T) {
 	documentService := &mocks.DocumentServiceServer{}
 	documentService.On("GeneratePreview", mock.AnythingOfType("v1.GeneratePreviewRequest"), mock.AnythingOfType("v1.DocumentService_GeneratePreviewServer")).Return(nil)
+	
 }
+*/
