cdff3e4a508c697091da984539fd46c45ad49b60
Nils Diekmann <Nils.Diekmann@lowellgroup.de>
2023-09-18 00:32:50 +0200

    - replace all logs with zerolog

--- internal/app/github/oauth/middleware.go
+++ internal/app/github/oauth/middleware.go
@@ -2,9 +2,7 @@ package oauth
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"path"
 	"strconv"
@@ -15,6 +13,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/github"
 	"github.com/google/uuid"
+	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/operation"
 	"golang.org/x/oauth2"
 	oauthgithub "golang.org/x/oauth2/github"
 )
@@ -33,7 +32,7 @@ func GithubOAuth() gin.HandlerFunc {
 		} else {
 			err := writeUserIdToContext(ctx, state, code)
 			if err != nil {
-				log.Println(err)
+				operation.Logger.Error().Err(err).Msg("Failed to write user id to context")
 				ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "user can not be unauthorized. refresh the page without code and state"})
 				return
 			}
@@ -106,12 +105,7 @@ func getUserId(ctx *gin.Context, state string, code string) (string, error) {
 		return "", fmt.Errorf("client.Users.Get() faled with '%s'", err.Error())
 	}
 
-	contents, err := json.MarshalIndent(user, "", "  ")
-	if err != nil {
-		return "", fmt.Errorf("json.MarshlIndent() failed with %s", err.Error())
-	}
-	fmt.Printf("User:\n%s\n", string(contents)) // TODO: Debug
-	fmt.Printf("User ID: %d", *user.ID)         // TODO: debug
+	operation.Logger.Debug().Msgf("User ID: %d, User: %s", *user.ID, user.String())
 	return strconv.FormatInt(*user.ID, 10), nil
 }
 
