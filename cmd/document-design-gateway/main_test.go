0fcccc5380a794586acffd8feda7327625921b90
Nils Diekmann <github@kinneko.de>
2023-10-24 00:29:39 +0200

    - test if the code really generate coverage

--- cmd/document-design-gateway/main_test.go
+++ cmd/document-design-gateway/main_test.go
@@ -1,17 +1,6 @@
 package main
 
-import (
-	"os"
-	"os/exec"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/document"
-	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/github/oauth"
-)
-
+/*
 func TestMain_GatewayConfigIsMissing(t *testing.T) {
 	if os.Getenv("EXECUTE") == "1" {
 		main()
@@ -43,3 +32,24 @@ func TestMain_OAuthConfigIsMissing(t *testing.T) {
 	exitCode := err.(*exec.ExitError).ExitCode()
 	assert.Equal(t, 1, exitCode)
 }
+*/
+
+/*
+func TestMain_ApplicationListenToInterrupt_GracefullShutdown(t *testing.T) {
+	if os.Getenv("EXECUTE") == "1" {
+		main()
+		return
+	}
+
+	t.Setenv(document.HostEnv, "http://localhost")
+	t.Setenv(document.PortEnv, "8080")
+	t.Setenv(oauth.ClientIdEnv, "1234567890")
+	t.Setenv(oauth.ClientSecretEnv, "1234567890")
+	cmd := exec.Command(os.Args[0], "-test.run=TestMain_ApplicationListenToInterrupt_GracefullShutdown")
+	cmd.Env = append(os.Environ(), "EXECUTE=1")
+	err := cmd.Run()
+	exitCode := err.(*exec.ExitError).ExitCode()
+	assert.Equal(t, 0, exitCode)
+	cmd.Process.Signal(syscall.SIGTERM)
+}
+*/
