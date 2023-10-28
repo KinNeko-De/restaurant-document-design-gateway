d98107170f99ef803024edd2199e98aa8bebe4e6
Nils Diekmann <github@kinneko.de>
2023-10-28 17:01:34 +0200

    Gracefull shutdown of http server (#7)
    - gracefull shutdown of http service
    - start http service with goroutine
    - specific exit codes in main

--- cmd/document-design-gateway/main.go
+++ cmd/document-design-gateway/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/document"
 	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/github/oauth"
@@ -9,27 +15,48 @@ import (
 
 func main() {
 	operation.SetDefaultLoggingLevel()
+	operation.Logger.Info().Msg("Starting application.")
 
 	documentServiceConfigError := document.ReadConfig()
 	if documentServiceConfigError != nil {
-		operation.Logger.Fatal().Err(documentServiceConfigError).Msg("Failed to read document service config")
+		operation.Logger.Error().Err(documentServiceConfigError).Msg("Failed to read document service config")
+		os.Exit(40)
 	}
 
 	oauthConfigError := oauth.ReadConfig()
 	if oauthConfigError != nil {
-		operation.Logger.Fatal().Err(oauthConfigError).Msg("Failed to read github oauth config")
+		operation.Logger.Error().Err(oauthConfigError).Msg("Failed to read github oauth config")
+		os.Exit(41)
 	}
 
-	StartHttpServer()
+	httpServerStop := make(chan struct{})
+	go startHttpServer(httpServerStop, ":8080")
+
+	<-httpServerStop
+	operation.Logger.Info().Msg("Application stopped.")
+	os.Exit(0)
 }
 
-func StartHttpServer() {
+func startHttpServer(httpServerStop chan struct{}, port string) {
 	router := setupRouter()
+	var gracefulStop = make(chan os.Signal, 1)
+	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
+	operation.Logger.Debug().Msgf("starting http server")
 
-	err := router.Run(":8080")
-	if err != nil {
-		operation.Logger.Fatal().Err(err).Msg("Failed to start http server")
+	server := &http.Server{Addr: port, Handler: router}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			operation.Logger.Error().Err(err).Msg("Failed to start http server")
+			os.Exit(50)
+		}
+	}()
+
+	stop := <-gracefulStop
+	if err := server.Shutdown(context.Background()); err != nil {
+		operation.Logger.Error().Err(err).Msg("Failed to shutdown http server")
 	}
+	operation.Logger.Debug().Msgf("http server stopped. Received signal %s", stop)
+	close(httpServerStop)
 }
 
 func setupRouter() *gin.Engine {
--- cmd/document-design-gateway/main_test.go
+++ cmd/document-design-gateway/main_test.go
@@ -3,13 +3,14 @@ package main
 import (
 	"os"
 	"os/exec"
+	"syscall"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"time"
 
 	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/document"
 	"github.com/kinneko-de/restaurant-document-design-gateway/internal/app/github/oauth"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMain_GatewayConfigIsMissing(t *testing.T) {
@@ -25,7 +26,7 @@ func TestMain_GatewayConfigIsMissing(t *testing.T) {
 	err := cmd.Run()
 	require.NotNil(t, err)
 	exitCode := err.(*exec.ExitError).ExitCode()
-	assert.Equal(t, 1, exitCode)
+	assert.Equal(t, 40, exitCode)
 }
 
 func TestMain_OAuthConfigIsMissing(t *testing.T) {
@@ -41,5 +42,62 @@ func TestMain_OAuthConfigIsMissing(t *testing.T) {
 	err := cmd.Run()
 	require.NotNil(t, err)
 	exitCode := err.(*exec.ExitError).ExitCode()
-	assert.Equal(t, 1, exitCode)
+	assert.Equal(t, 41, exitCode)
+}
+
+// test does not run on windows
+// In case you broke something, the test will run forever
+// In the pipeline you will see:
+// panic: test timed out after 5m0s
+// running tests:
+// TestMain_ApplicationListenToInterrupt_GracefullShutdown (5m0s)
+func TestMain_ApplicationListenToSIGTERM_AndGracefullyShutdown(t *testing.T) {
+	if os.Getenv("EXECUTE") == "1" {
+		main()
+		return
+	}
+
+	t.Setenv(document.HostEnv, "http://localhost")
+	t.Setenv(document.PortEnv, "8080")
+	t.Setenv(oauth.ClientIdEnv, "1234567890")
+	t.Setenv(oauth.ClientSecretEnv, "1234567890")
+	cmd := exec.Command(os.Args[0], "-test.run=TestMain_ApplicationListenToSIGTERM_AndGracefullyShutdown")
+	cmd.Env = append(os.Environ(), "EXECUTE=1")
+	err := cmd.Start()
+	require.Nil(t, err)
+	time.Sleep(1 * time.Second)
+	cmd.Process.Signal(syscall.SIGTERM)
+	err = cmd.Wait()
+	require.Nil(t, err)
+	exitCode := cmd.ProcessState.ExitCode()
+	assert.Equal(t, 0, exitCode)
+}
+
+// test does not run on windows
+// In case you broke something, the test will run forever
+// In the pipeline you will see:
+// panic: test timed out after 5m0s
+// running tests:
+// TestMain_ApplicationListenToInterrupt_GracefullShutdown (5m0s)
+func TestMain_ProcessAlreadyListenToPort_AppCrash(t *testing.T) {
+	if os.Getenv("EXECUTE") == "1" {
+		main()
+		return
+	}
+
+	t.Setenv(document.HostEnv, "http://localhost")
+	t.Setenv(document.PortEnv, "8080")
+	t.Setenv(oauth.ClientIdEnv, "1234567890")
+	t.Setenv(oauth.ClientSecretEnv, "1234567890")
+	blockingcmd := exec.Command(os.Args[0], "-test.run=TestMain_ProcessAlreadyListenToPort_AppCrash")
+	blockingcmd.Env = append(os.Environ(), "EXECUTE=1")
+	blockingErr := blockingcmd.Start()
+	require.Nil(t, blockingErr)
+	cmd := exec.Command(os.Args[0], "-test.run=TestMain_ProcessAlreadyListenToPort_AppCrash")
+	cmd.Env = append(os.Environ(), "EXECUTE=1")
+	err := cmd.Run()
+	require.NotNil(t, err)
+	exitCode := err.(*exec.ExitError).ExitCode()
+	assert.Equal(t, 50, exitCode)
+	blockingcmd.Process.Kill()
 }
