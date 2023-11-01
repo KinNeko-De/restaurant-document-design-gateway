42207649d4035a496efd9b235bb46b64deccd30b
Nils Diekmann <github@kinneko.de>
2023-11-01 17:56:18 +0100

    - add commands to cheht

--- scripts/microk8s/pipeline.sh
+++ scripts/microk8s/pipeline.sh
@@ -1,8 +1,12 @@
 cd .. && cd ..
 
+go build -o ./bin/app ./cmd/document-design-gateway/main.go
+
 sudo docker build -f ./build/dockerfile . -t restaurant-document-design-gateway:latest
 sudo docker image ls
 sudo docker tag restaurant-document-design-gateway localhost:32000/restaurant-document-design-gateway:latest
 sudo docker push localhost:32000/restaurant-document-design-gateway:latest
 
 kubectl apply --kustomize ./deployment/microk8s/overlays/dev
+
+# kubectl logs -l app=document-design-gateway -n restaurant
