26065e9d09199da5117b1dfbe8b2cf9cf4a6fabe
Nils Diekmann <github@kinneko.de>
2023-11-11 18:37:41 +0100

    - move tests

---
+++ internal/app/server/http_test.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStartHttpServer_ProcessAlreadyListenToPort_AppCrash(t *testing.T) {
+	if os.Getenv("EXECUTE") == "1" {
+		StartHttpServer(make(chan struct{}), make(chan struct{}), ":8080")
+		return
+	}
+
+	blockingcmd := exec.Command(os.Args[0], "-test.run=TestStartHttpServer_ProcessAlreadyListenToPort_AppCrash")
+	blockingcmd.Env = append(os.Environ(), "EXECUTE=1")
+	blockingErr := blockingcmd.Start()
+	defer blockingcmd.Process.Kill()
+	require.Nil(t, blockingErr)
+
+	time.Sleep(time.Second * 1)
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestStartHttpServer_ProcessAlreadyListenToPort_AppCrash")
+	cmd.Env = append(os.Environ(), "EXECUTE=1")
+	err := cmd.Run()
+	require.NotNil(t, err)
+	exitCode := err.(*exec.ExitError).ExitCode()
+	assert.Equal(t, 50, exitCode)
+
+}
+
+func TestStartHttpServer_PortMalformed(t *testing.T) {
+	if os.Getenv("EXECUTE") == "1" {
+		StartHttpServer(make(chan struct{}), make(chan struct{}), "malformedPort")
+		return
+	}
+
+	runningApp := exec.Command(os.Args[0], "-test.run=TestStartHttpServer_PortMalformed")
+	runningApp.Env = append(os.Environ(), "EXECUTE=1")
+	err := runningApp.Run()
+	require.NotNil(t, err)
+	exitCode := err.(*exec.ExitError).ExitCode()
+	assert.Equal(t, 50, exitCode)
+}
